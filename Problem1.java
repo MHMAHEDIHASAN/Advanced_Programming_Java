e828fac2d930e568903e90fae4f53a9544566ae1
Md.Mahedi Hasan <147182693+MHMAHEDIHASAN@users.noreply.github.com>
2024-09-15 21:37:29 +0600

    Java

---
+++ Problem1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class problem1{
+public static void main(String[] args){
+Scanner input = new Scanner(System.in);
+int num1,num2;
+num1 = input.nextInt();
+num2 = input.nextInt();
+int sum = (num1+num2);
+int mul = (num1+num2);
+double div = (num1/num2);
+System.out.println("SUM : "+sum);
+System.out.println("MUL : "+mul);
+System.out.println("DIV : "+div);
+}
+}
